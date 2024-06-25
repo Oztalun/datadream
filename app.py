401462949a558afb4a783e0b1c5d6be225ab4c65
Kimchi-mat-coffee <hangyaku0955@naver.com>
2024-06-25 17:56:05 +0900

    <백두산>

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
-#가상환경 설정, pip install flask flask_sqlalchemy 다운로드
+# 가상환경 설정, pip install flask flask_sqlalchemy 다운로드
+from flask_sqlalchemy import SQLAlchemy
+import os
 from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
-#DB기본 코드----------------------------------------------------
-import os
-from flask_sqlalchemy import SQLAlchemy
+# DB기본 코드----------------------------------------------------
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] =\
-        'sqlite:///' + os.path.join(basedir, 'database.db')
+    'sqlite:///' + os.path.join(basedir, 'database.db')
 
 db = SQLAlchemy(app)
+
 
 class Song(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,14 +24,16 @@ class Song(db.Model):
     def __repr__(self):
         return f'{self.username} {self.title} 추천 by {self.username}'
 
+
 with app.app_context():
     db.create_all()
-#-----------------------------------------------------------------
+# -----------------------------------------------------------------
+
 
 @app.route("/")
 def home():
     name = '데이터드림'
-    motto = "행복해서 웃는게 아니라 웃어서 행복합니다."
+    motto = "메인페이지에 올라갈 내용을 적어주세요!-백두산 수정"
 
     context = {
         "name": name,
@@ -38,15 +41,18 @@ def home():
     }
     return render_template('motto.html', data=context)
 
-@app.route("/Introduce/")# 경로 바꾸면 redirect경로도 바꿔주기
+
+@app.route("/Introduce/")  # 경로 바꾸면 redirect경로도 바꿔주기
 def music():
     song_list = Song.query.all()
-    return render_template('Introduce.html', data = song_list)
+    return render_template('Introduce.html', data=song_list)
+
 
 @app.route("/music/<username>")
 def render_music_filter(username):
     filter_list = Song.query.filter_by(username=username).all()
-    return render_template('Introduce.html', data = filter_list)
+    return render_template('Introduce.html', data=filter_list)
+
 
 @app.route("/music/create/")
 def music_create():
@@ -55,10 +61,12 @@ def music_create():
     artist_receive = request.args.get("artist")
     image_receive = request.args.get("image_url")
 
-    song = Song(username = username_receive, title = title_receive, artist = artist_receive, image_url = image_receive)
+    song = Song(username=username_receive, title=title_receive,
+                artist=artist_receive, image_url=image_receive)
     db.session.add(song)
     db.session.commit()
     return redirect(url_for('Introduce'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
